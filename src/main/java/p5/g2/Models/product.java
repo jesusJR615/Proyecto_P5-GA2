8b8ac818cf89e10d11e4a8ba4dfc75edd1432477
jesusJR615 <martinblazquezjesus@gmail.com>
2026-09-26 17:51:03 +0200

    clase product finalizada

--- src/main/java/p5/g2/Models/product.java
+++ src/main/java/p5/g2/Models/product.java
@@ -3,6 +3,7 @@ package p5.g2.models;
 
 public class product {
     //Declaro los atributos de la clase product
+    private int id;
     private String name;
     private double price;
     private String description;
@@ -12,13 +13,22 @@ public class product {
     }
 
     //Creo el contructor
-    public product(String name, double price, String description) {
+    public product(int id, String name, double price, String description) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
     //Creo los metodos get y set de cada atributo
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
